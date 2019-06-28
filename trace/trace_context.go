35566c85c8e764a89ddd32ac755a47d7b666a4a8
Isobel Redelmeier <iredelmeier@gmail.com>
2019-06-27 19:09:19 -0700

    Add trace.TraceContextFromContext
    (What a name!)

--- trace/trace_context.go
+++ trace/trace_context.go
@@ -1,6 +1,32 @@
 package trace
 
+import (
+	"context"
+
+	"github.com/iredelmeier/opentelemetry-playground/trace/internal"
+)
+
+const (
+	SpanIDSize  = 8
+	TraceIDSize = 16
+)
+
+type SpanID [SpanIDSize]byte
+
+type TraceID [TraceIDSize]byte
+
 type TraceContext struct {
 	TraceID TraceID
 	SpanID  SpanID
+}
+
+func TraceContextFromContext(ctx context.Context) (TraceContext, bool) {
+	if span, ok := internal.SpanFromContext(ctx); ok {
+		return TraceContext{
+			TraceID: span.TraceID(),
+			SpanID:  span.ID(),
+		}, true
+	}
+
+	return TraceContext{}, false
 }
